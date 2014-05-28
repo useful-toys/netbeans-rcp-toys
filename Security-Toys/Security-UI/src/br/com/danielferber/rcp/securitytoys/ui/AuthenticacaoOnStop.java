ffd9e6f1fdf5237d850a2af3e83b822776352eed
Daniel Felix Ferber <danielferber@yahoo.com.br>
2014-05-28 08:21:41 -0300

    [dev] UI with logoff message. [dev] Confirm logoff if there is an authenticated user.

---
+++ Security-Toys/Security-UI/src/br/com/danielferber/rcp/securitytoys/ui/AuthenticacaoOnStop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.danielferber.rcp.securitytoys.ui;
+
+import br.com.danielferber.rcp.securitytoys.api.SegurancaService;
+import java.util.concurrent.Callable;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.modules.OnStop;
+
+@OnStop
+public class AuthenticacaoOnStop implements Callable<Boolean> {
+
+    @Override
+    public Boolean call() throws Exception {
+        if (SegurancaService.getDefault().getUsuario() != null) {
+            NotifyDescriptor d = new NotifyDescriptor("Finalizar aplicação?", "Pergunta",
+                    NotifyDescriptor.YES_NO_OPTION, NotifyDescriptor.QUESTION_MESSAGE,
+                    null, null);
+            return DialogDisplayer.getDefault().notify(d) == NotifyDescriptor.YES_OPTION;        
+        }
+        return true;
+    }
+
+}
