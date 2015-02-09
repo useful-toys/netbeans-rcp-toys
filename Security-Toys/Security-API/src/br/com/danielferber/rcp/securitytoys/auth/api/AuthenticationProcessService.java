ab867ee04f4338e7deb5af2a78cf47be02819cae
Daniel Felix Ferber <danielferber@gmail.com>
2015-02-09 08:40:45 -0200

    [dev] Translate

--- Security-Toys/Security-API/src/br/com/danielferber/rcp/securitytoys/auth/api/AuthenticationProcessService.java
+++ Security-Toys/Security-API/src/br/com/danielferber/rcp/securitytoys/auth/api/AuthenticationProcessService.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.danielferber.rcp.securitytoys.auth.api;
 
 import br.com.danielferber.rcp.securitytoys.api.AuthenticatedUser;
