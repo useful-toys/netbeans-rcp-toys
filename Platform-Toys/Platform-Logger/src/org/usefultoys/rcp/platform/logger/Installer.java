6e7510f83c04d817899a9f949245e4065d461776
Daniel Felix Ferber <x7ws>
2015-09-20 20:10:34 -0300

    [org] Moved logger configuration module to Platform suite. [devl Platform-Logger creates sample logger file if it does not exist.

--- Platform-Toys/Platform-Logger/src/org/usefultoys/rcp/platform/logger/Installer.java
+++ Platform-Toys/Platform-Logger/src/org/usefultoys/rcp/platform/logger/Installer.java
@@ -1,9 +1,10 @@
 /*
  */
-package org.usefultoys.rcp.logger.configuration;
+package org.usefultoys.rcp.platform.logger;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.logging.Handler;
@@ -17,24 +18,32 @@ import org.openide.modules.OnStart;
 @OnStart
 public final class Installer implements Runnable {
 
+    static final String PROPERTIES_FILE_NAME = "logger.properties";
+    static final String FILE_OBJECT_NAME = "Logger";
+
     static void readLoggerLevelFromProperties(Properties properties) {
         for (String name : properties.stringPropertyNames()) {
-            final Object valor = properties.getProperty(name);
-            applyLoggerLevel(name, valor);
+            final Object levelStr = properties.getProperty(name);
+            if (name.endsWith(".level")) {
+                final String loggerName = name.substring(0, name.length() - 6);
+                applyLoggerLevel(loggerName, levelStr);
+            } else {
+                applyLoggerLevel(name, levelStr);
+            }
         }
     }
 
     static void readLoggerLevelFromFileObject(FileObject loggerFO) {
         final Enumeration<String> attributeEnum = loggerFO.getAttributes();
         while (attributeEnum.hasMoreElements()) {
-            final String nome = attributeEnum.nextElement();
-            final Object valor = loggerFO.getAttribute(nome);
-            applyLoggerLevel(nome, (String) valor);
+            final String loggerName = attributeEnum.nextElement();
+            final Object levelStr = loggerFO.getAttribute(loggerName);
+            applyLoggerLevel(loggerName, (String) levelStr);
         }
     }
 
     static void resetHandlerLevel() {
-        Logger rootLogger = Logger.getLogger("");
+        final Logger rootLogger = Logger.getLogger("");
         for (Handler handler : rootLogger.getHandlers()) {
             try {
                 handler.setLevel(Level.ALL);
@@ -93,29 +102,47 @@ public final class Installer implements Runnable {
     public void run() {
         resetHandlerLevel();
 
+        readLayerFileObject();
+        readUserConfigurationPropertiesFile();
+
+        resetLogManager();
+    }
+
+    static void readLayerFileObject() {
         final FileObject configRoot = FileUtil.getConfigRoot();
-        FileObject loggerFile = configRoot.getFileObject("Logger");
-        try {
-            if (loggerFile == null) {
-                loggerFile = configRoot.createData("Logger");
+        FileObject loggerFile = configRoot.getFileObject(FILE_OBJECT_NAME);
+        if (loggerFile == null) {
+            try {
+                loggerFile = configRoot.createData(FILE_OBJECT_NAME);
+                loggerFile.setAttribute(getExampleLoggerName(), "INFO");
+            } catch (IOException e) {
+                Logger.getLogger("").log(Level.WARNING, "Failed to create 'Logger' file in system filesystem.", e);
             }
-        } catch (IOException e) {
-            Logger.getLogger("").log(Level.WARNING, "Failed to create 'Logger' file in system filesystem.", e);
         }
-
-        FileObject loggerPropertiesFile = configRoot.getFileObject("logger.properties");
-        try {
-            if (loggerPropertiesFile == null) {
-                loggerPropertiesFile = configRoot.createData("logger", "properties");
-            }
-        } catch (IOException e) {
-            Logger.getLogger("").log(Level.WARNING, "Failed to create logger.properties file in configuration directory.", e);
-        }
-
         if (loggerFile != null) {
             readLoggerLevelFromFileObject(loggerFile);
         }
+    }
 
+    private static String getExampleLoggerName() {
+        return Installer.class.getPackage().getName() + ".example";
+    }
+
+    static void readUserConfigurationPropertiesFile() {
+        final FileObject configRoot = FileUtil.getConfigRoot();
+        FileObject loggerPropertiesFile = configRoot.getFileObject(PROPERTIES_FILE_NAME);
+        if (loggerPropertiesFile == null) {
+            try {
+                loggerPropertiesFile = configRoot.createData(PROPERTIES_FILE_NAME);
+                OutputStream os = loggerPropertiesFile.getOutputStream();
+                Properties properties = new Properties();
+                properties.put(getExampleLoggerName(), "INFO");
+                properties.store(os, "Example logger configuration");
+                os.close();
+            } catch (IOException e) {
+                Logger.getLogger("").log(Level.WARNING, "Failed to create logger.properties file in configuration directory.", e);
+            }
+        }
         if (loggerPropertiesFile != null) {
             Properties properties = new Properties();
             try {
@@ -127,8 +154,6 @@ public final class Installer implements Runnable {
             }
             readLoggerLevelFromProperties(properties);
         }
-
-        resetLogManager();
     }
 
 }
