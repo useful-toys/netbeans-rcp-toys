bd4c50934464b9fb44db9a2ce2238c1bdca3d7c5
Daniel Felix Ferber <x7ws>
2015-09-27 18:24:13 -0300

    [fix] Fixed authentication flow if maximal number of tries is exceeded.

--- Platform-Toys/Platform-Security-UI/src/org/usefultoys/rcp/security/core/AuthenticationProcessServiceDefault.java
+++ Platform-Toys/Platform-Security-UI/src/org/usefultoys/rcp/security/core/AuthenticationProcessServiceDefault.java
@@ -27,8 +27,7 @@ import org.openide.util.NbBundle;
     "AuthenticationProcessServiceDefault_Message_InactiveUser=User is inactive",
     "AuthenticationProcessServiceDefault_Message_UnavailableService=Authentication is unavailable",
     "AuthenticationProcessServiceDefault_Message_Exceeded=Exceeded allowed number of tries",
-    "AuthenticationProcessServiceDefault_Message_Canceled=Login canceled.",
-})
+    "AuthenticationProcessServiceDefault_Message_Canceled=Login canceled.",})
 public class AuthenticationProcessServiceDefault implements AuthenticationProcessService {
 
     public static final Logger LOGGER = Logger.getLogger(AuthenticationProcessServiceDefault.class.getName());
@@ -51,20 +50,26 @@ public class AuthenticationProcessServiceDefault implements AuthenticationProces
         final AuthenticatedUser authenticatedUser = nbc.editAndProcess(() -> {
             final CredentialPanel.Outbound outbound = nbc.getOutbound();
             outbound.tries++;
-            if (outbound.tries > MAXIMAL_NUMER_TRIES) {
-                return null;
-            }
             nbc.getDialogState().changeToInfoState(Bundle.AuthenticationProcessServiceDefault_Message_Working());
             try {
                 return SecurityService.getDefault().login(outbound.login, outbound.password);
             } catch (AuthenticationException.IncorrectCredentials e) {
                 nbc.getDialogState().changeToErrorState(Bundle.AuthenticationProcessServiceDefault_Message_IncorrectCredentials());
+                if (outbound.tries > MAXIMAL_NUMER_TRIES) {
+                    return null;
+                } 
                 throw new NetbeansDialogConvention.PreventClose();
             } catch (AuthenticationException.InexistingUser e) {
                 nbc.getDialogState().changeToErrorState(Bundle.AuthenticationProcessServiceDefault_Message_InexistingUser());
+                if (outbound.tries > MAXIMAL_NUMER_TRIES) {
+                    return null;
+                }
                 throw new NetbeansDialogConvention.PreventClose();
             } catch (AuthenticationException.InactiveUser e) {
                 nbc.getDialogState().changeToErrorState(Bundle.AuthenticationProcessServiceDefault_Message_InactiveUser());
+                if (outbound.tries > MAXIMAL_NUMER_TRIES) {
+                    return null;
+                }
                 throw new NetbeansDialogConvention.PreventClose();
             } catch (Exception e) {
                 LOGGER.log(Level.SEVERE, Bundle.AuthenticationProcessServiceDefault_Message_UnavailableService(), e);
@@ -73,13 +78,13 @@ public class AuthenticationProcessServiceDefault implements AuthenticationProces
             }
         });
         if (nbc.getOutbound().tries > MAXIMAL_NUMER_TRIES) {
-            NotifyDescriptor d = new DialogDescriptor.Message(Bundle.AuthenticationProcessServiceDefault_Message_Exceeded(), DialogDescriptor.INFORMATION_MESSAGE);
-            DialogDisplayer.getDefault().notify(d);
+//            NotifyDescriptor d = new DialogDescriptor.Message(Bundle.AuthenticationProcessServiceDefault_Message_Exceeded(), DialogDescriptor.INFORMATION_MESSAGE);
+//            DialogDisplayer.getDefault().notify(d);
             throw new AuthenticationProcessException.Exceeded();
         }
         if (authenticatedUser == null) {
-            NotifyDescriptor d = new DialogDescriptor.Message(Bundle.AuthenticationProcessServiceDefault_Message_Canceled(), DialogDescriptor.INFORMATION_MESSAGE);
-            DialogDisplayer.getDefault().notify(d);
+//            NotifyDescriptor d = new DialogDescriptor.Message(Bundle.AuthenticationProcessServiceDefault_Message_Canceled(), DialogDescriptor.INFORMATION_MESSAGE);
+//            DialogDisplayer.getDefault().notify(d);
             throw new AuthenticationProcessException.Canceled();
         }
         return authenticatedUser;
